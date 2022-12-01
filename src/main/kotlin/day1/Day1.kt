c0e51dedfcf623ff371684b176859010ef1f1b03
lafie-rage <corentin.destrez62@gmail.com>
2022-12-01 21:08:30 +0100

    Finishing part 2 of the Day 1

--- src/main/kotlin/day1/Day1.kt
+++ src/main/kotlin/day1/Day1.kt
@@ -4,9 +4,25 @@ import java.io.File
 
 fun main() {
     println("Part 1 :")
-    part1("./src/main/resources/day1/input.txt")
+    val resultPart1 = part1("./src/main/resources/day1/input.txt")
+    println("Answer : $resultPart1")
+
+    println("Part 2 :")
+    val resultPart2 = part2("./src/main/resources/day1/input.txt")
+    println("Answer : $resultPart2")
 }
-fun part1(path : String) {
+fun part1(path : String): Int {
+    val elvesCalories = decodeFile(path)
+
+    return elvesCalories.maxOrNull()!!
+}
+
+fun part2(path: String): Int {
+    val elvesCalories = decodeFile(path).sortedDescending()
+
+    return elvesCalories[0] + elvesCalories[1] + elvesCalories[2]
+}
+private fun decodeFile(path: String): List<Int> {
     val elvesCalories = mutableListOf<Int>()
 
     val elves = File(path).readText().split("\n\n")
@@ -18,5 +34,5 @@ fun part1(path : String) {
         }
     }
 
-    println("\tAnswer : ${elvesCalories.maxOrNull()}")
+    return elvesCalories
 }
--- src/test/kotlin/day1/Day1.kt
+++ src/test/kotlin/day1/Day1.kt
@@ -1,12 +1,20 @@
 package day1
 
 import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
 
 class Day1 {
 
     @Test
     fun `test part 1`() {
-        part1("./src/test/resources/day1/input.txt")
+        val result = part1("./src/test/resources/day1/input.txt")
+        assertEquals(24000, result)
+    }
+
+    @Test
+    fun `test part 2`() {
+        val result = part2("./src/test/resources/day1/input.txt")
+        assertEquals(45000, result)
     }
 
 }
