dfd393bac166df8f9a357f00e9b6cab7fccaff4f
Lafie-rage <corentin.destrez62@gmail.com>
2022-12-03 12:36:34 +0100

    Day 2 part 2

--- src/main/kotlin/day2/Day2.kt
+++ src/main/kotlin/day2/Day2.kt
@@ -7,27 +7,43 @@ fun main() {
     val resultPart1 = part1("./src/main/resources/day2/input.txt")
     println("Answer : $resultPart1")
 
-    /*println("Part 2 :")
+    println("Part 2 :")
     val resultPart2 = part2("./src/main/resources/day2/input.txt")
-    println("Answer : $resultPart2")*/
+    println("Answer : $resultPart2")
 }
 
 fun part1(path: String): Int {
     val tournament = decodeFile(path)
-    val matchsScores = mutableListOf<Int>()
+    val matchesScores = mutableListOf<Int>()
     tournament.forEach { match ->
         match.split(" ").let {
-            matchsScores.add(decodeSingleLine_part1(it[0][0], it[1][0]))
+            matchesScores.add(decodeSingleLinePart1(it[0][0], it[1][0]))
         }
     }
-    return matchsScores.sum()
+    return matchesScores.sum()
 }
-private fun decodeSingleLine_part1(player1Shape: Char, player2Shape: Char): Int =  shapeForOpponent(player1Shape).against(shapeForMe(player2Shape))
+
+fun part2(path: String): Int {
+    val tournament = decodeFile(path)
+    val matchesScores = mutableListOf<Int>()
+    tournament.forEach { match ->
+        match.split(" ").let {
+            matchesScores.add(decodeSingleLinePart2(it[0][0], it[1][0]))
+        }
+    }
+    return matchesScores.sum()
+}
+
+private fun decodeSingleLinePart1(opponentShape: Char, myShape: Char): Int =  shapeForOpponent(opponentShape).against(shapeForMe(myShape))
+
+private fun decodeSingleLinePart2(opponentEncryptedShape: Char, intendedResult: Char): Int {
+    val opponentShape = shapeForOpponent(opponentEncryptedShape)
+    return opponentShape.against(shapeForIntendedResult(intendedResult, opponentShape))
+}
 
 private fun decodeFile(path: String): List<String> {
     return File(path).readLines()
 }
-
 
 private enum class Shape(
     val score: Int,
@@ -45,6 +61,9 @@ private fun shapeForOpponent(opponentShape: Char): Shape {
     }
 }
 
+// X : Rock
+// Y : Paper
+// Z : Scissors
 private fun shapeForMe(myShape: Char): Shape =
     when (myShape) {
         'X' -> Shape.Rock
@@ -52,11 +71,25 @@ private fun shapeForMe(myShape: Char): Shape =
         else -> Shape.Scissors
     }
 
-private fun Shape.against(shape: Shape): Int =
+// X : loose
+// Y : draw
+// Z : win
+private fun shapeForIntendedResult(intendedResult: Char, opponentShape: Shape): Shape =
     when {
-        this == shape -> 3
-        this == Shape.Scissors && shape == Shape.Rock ||
-                this == Shape.Rock && shape == Shape.Paper ||
-                this == Shape.Paper && shape == Shape.Scissors -> 6
-        else -> 0
-    } + shape.score
+        intendedResult == 'X' && opponentShape == Shape.Rock ||
+                intendedResult == 'Y' && opponentShape == Shape.Scissors ||
+                intendedResult == 'Z' && opponentShape == Shape.Paper -> Shape.Scissors
+        intendedResult == 'X' && opponentShape == Shape.Paper ||
+                intendedResult == 'Y' && opponentShape == Shape.Rock ||
+                intendedResult == 'Z' && opponentShape == Shape.Scissors -> Shape.Rock
+        else -> Shape.Paper
+    }
+
+private fun Shape.against(myShape: Shape): Int =
+    when {
+        this == myShape -> 3 // Draw
+        this == Shape.Scissors && myShape == Shape.Rock ||
+                this == Shape.Rock && myShape == Shape.Paper ||
+                this == Shape.Paper && myShape == Shape.Scissors -> 6 // Win
+        else -> 0 // Loose
+    } + myShape.score
--- src/test/kotlin/day2/Day2Test.kt
+++ src/test/kotlin/day2/Day2Test.kt
@@ -11,4 +11,11 @@ class Day2Test {
 
         assertEquals(15, result)
     }
+
+    @Test
+    fun `test part 2`() {
+        val result = part2("./src/test/resources/day2/input.txt")
+
+        assertEquals(12, result)
+    }
 }
